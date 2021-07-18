b8afed042423f3ba4ba670c6e91b9de14b865c91
Zar Ni Myo <zarnimyo@sourcedinternational.com>
2021-07-18 17:05:51 +0630

    Add project files.

---
+++ ShoppingCart/Services/CartService.cs
@@ -0,0 +1,37 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Services
+{
+    public class CartService : ICartService
+    {
+        public CartService()
+        {
+            Cart = new List<Product>();
+        }
+        public IList<Product> Cart { get; private set; }
+
+        public decimal Total { get; set; }
+
+        public event Action OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AddProduct(Product product)
+        {
+            Cart.Add(product);
+            Total += product.Price;
+            NotifyStateChanged();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            Cart.Remove(product);
+            Total -= product.Price;
+            NotifyStateChanged();
+        }
+    }
+}
---
+++ ShoppingCart/Services/ICartService.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ShoppingCart.Services
+{
+    public interface ICartService
+    {
+        IList<Product> Cart { get; }
+        decimal Total { get; set; }
+
+        event Action OnChange;
+
+        void AddProduct(Product product);
+        void DeleteProduct(Product product);
+    }
+}
